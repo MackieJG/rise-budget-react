78ae350aa8349ebf491778a7646cc2a6ff0def2e
MackieJG <joshuamackie10@googlemail.com>
2023-03-01 13:41:25 +0000

    creating pot container/list/detail and linking it to the back end.

--- src/components/expenses/ExpenseList.tsx
+++ src/components/expenses/ExpenseList.tsx
@@ -24,7 +24,6 @@ interface PotProps {
     title: string;
     amount: BigInt;
     user: any;
-
 }
 
 const ExpenseList = ({expenses}: ExpenseListProps ) => {
--- src/components/pots/PotList.tsx
+++ src/components/pots/PotList.tsx
@@ -1,1 +1,37 @@
 import React from "react";
+import PotDetail from "./PotDetail";
+
+interface PotListProps {
+    pots: PotProps[];
+}
+
+interface PotProps {
+    title: string;
+    amount: BigInt;
+    user: any;
+}
+
+const PotList = ({pots}: PotListProps ) => {
+    if(pots.length === 0 ) {
+        return(<p>You have no pots! Lets get you started!</p>)
+    }
+
+    const potElements = pots.map((pot, index) => {
+        return (
+            <li key={index} className="pot-item">
+                <div className ="pots">
+                    <PotDetail pots={pots} />
+                </div>
+            </li>
+        )
+    })
+
+    return (
+        <>
+        <ul className = "pot-list">
+            {potElements}
+        </ul>
+        </>
+    )
+}
+export default PotList;
