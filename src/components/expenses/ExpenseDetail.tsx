ba1ee94ae1c44332e17c79942c1035561ffb9dee
MackieJG <joshuamackie10@googlemail.com>
2023-03-07 11:03:43 +0000

    updating sweet alerts for add expense and delete expense

--- src/components/expenses/ExpenseDetail.tsx
+++ src/components/expenses/ExpenseDetail.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
 import Expense from './Expense';
- 
+import Swal from 'sweetalert2'
+
 enum CategoryType {
     GROCERIES = "GROCERIES",
     UTILITIES = "UTILITIES",
